80d796f6188a48fe0eea27d838e83264d0054a75
munishkumarm <98474339+munishkumarm@users.noreply.github.com>
2023-09-17 21:09:04 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,6 @@ then
   echo "Docker Exists"
 else
   echo "No Docker Container Runnig on that name"
+  sh "docker build -t munish/jenkins-learn ."
+  sh "docker run -dp 3500:3500 munish/jenkins-learn"
 fi
