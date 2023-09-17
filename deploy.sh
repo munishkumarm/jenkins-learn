9028722d484e806d5614ebeb3e7b1e98e07e9c48
munishkumarm <98474339+munishkumarm@users.noreply.github.com>
2023-09-17 22:06:37 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 dockerExits = docker ps | grep "jenkins-learn" -c
-if [ dockerExist -ge 0 ]
+if ! [ $dockerExist == 0 ]
 then
   echo "Docker Exists"
 else
   echo "No Docker Container Runnig on that name"
-  sh "docker build -t munish/jenkins-learn ."
-  sh "docker run -dp 3500:3500 munish/jenkins-learn"
+pwd 
+ docker build -t munish/jenkins-learn .
+  docker run -dp 3500:3500 munish/jenkins-learn
+  docker ps
 fi
