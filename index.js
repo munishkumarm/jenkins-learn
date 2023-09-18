d41d032e80226f4e2174252b61b035799fa81a6c
munishkumarm <98474339+munishkumarm@users.noreply.github.com>
2023-09-18 12:30:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
+app.get("/:name", (req, res) => {
+    res.send("Hello World " + req.params.name)
+})
+
 app.listen(3500, () => {
     console.log('App running on Port : 3500')
 })
