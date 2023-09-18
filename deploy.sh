a233b9cad2b6ec83157722258a7f7d78c530ec6f
munishkumarm <98474339+munishkumarm@users.noreply.github.com>
2023-09-18 12:29:36 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,21 @@
-#!/bin/bash
-dockerExits = docker ps | grep "jenkins-learn" -c
-if ! [ $dockerExist == 0 ]
+#!/bin/sh
+DE=$(docker ps | grep "jenkinslearndocker1" -c)
+#echo $DE
+if [ $DE > 0 ];
 then
-  echo "Docker Exists"
+  echo "Docker Running"
+  echo "STOPPING RUNNING CONTAINER"
+  docker stop jenkinslearndocker1
+  echo "CONTAINER STOPPED"
+  echo "UPDATING IMAGE WITH LATEST CHANGES"
+  docker build -t munish/jenkins-learn .
+  echo "CHNAGES UPDATED"
+  echo "RUN CONTAINER"
+  docker run --name jenkinslearndocker1 -dp 3500:3500 munish/jenkins-learn
+  echo "DEPLOYMENT SUCCESS"
 else
   echo "No Docker Container Runnig on that name"
-   docker build -t munish/jenkins-learn .
-    docker run -dp 3500:3500 munish/jenkins-learn
-    docker ps
+  docker build -t munish/jenkins-learn .
+  docker run --name jenkinslearndocker1 -dp 3500:3500 munish/jenkins-learn
+  docker ps
 fi
